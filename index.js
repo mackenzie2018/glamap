c67af5dac14f701ecde2b16d36b304057342eef0
Calum Mackenzie <cmacdotdev@proton.me>
2026-04-05 16:54:48 +0100

    succsessfully use wasm to control shape rendering

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
 var gl = null;
+var memory = null;
+var vaos = [];
+var programs = [];
 
 function loadShader(gl, type, source) {
+  // console.log('Type of gl is: ', typeof(gl), gl);
   const shader = gl.createShader(type);
-
-  // Send the source to the shader object
 
   gl.shaderSource(shader, source);
 
-  // Compile the shader program
-
   gl.compileShader(shader);
-
-  // See if it compiled successfully
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
     alert(
       `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`,
     );
     gl.deleteShader(shader);
-    return null;
-  }
-
+    return null; 
+  } 
   return shader;
 }
 
+function compileLinkProgramWasm(vs, vs_len, fs, fs_len) {
+  const vs_source = new Uint8Array(memory.buffer, vs, vs_len)
+  const fs_source = new Uint8Array(memory.buffer, fs, fs_len)
+  const dec = new TextDecoder('utf-8');
+  const program = compileLinkProgram(dec.decode(vs_source), dec.decode(fs_source));
+  programs.push(program);
+  return programs.length - 1;
+}
 
-function compileLinkProgram(gl, vertex_source, fragment_source) {
+
+function compileLinkProgram(vertex_source, fragment_source) {
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertex_source);
   const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragment_source);
 
   // Create the shader program
 
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
 
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
     alert(
       `Unable to initialize the shader program: ${gl.getProgramInfoLog(
-        program,
+        shaderProgram,
       )}`,
     );
   }
-  return program
+  return shaderProgram
 }
 
 
@@ -60,7 +66,7 @@ function bind2DFloat32Data(positions, vsSource, fsSource) {
   // Now pass the list of positions into WebGL to build the
   // shape. We do this by creating a Float32Array from the
   // JavaScript array, then use it to fill the current buffer.
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
   const vao = gl.createVertexArray()
   gl.bindVertexArray(vao);
   const vertex_attrib = 0;
@@ -83,55 +89,70 @@ function bind2DFloat32Data(positions, vsSource, fsSource) {
 }
 
 
+function bind2DFloat32DataWasm(d, len) {
+  const arr = new Float32Array(memory.buffer, d, len);
+  vaos.push(bind2DFloat32Data(arr));
+  return vaos.length - 1;
+}
+
+
 
 async function init() {
-  // const importObject = {
-  //   env: {
-  //     // Add any functions your Zig code imports here
-  //     "add",
-  //   },
-  // };
-  const mod = await WebAssembly.instantiateStreaming(fetch("/zig-out/bin/index.wasm"), {});
-  console.log(mod.instance.exports.add(1, 2));
-  const response = await fetch("/zig-out/bin/index.wasm");
-  const buffer = await response.arrayBuffer();
-  const module = await WebAssembly.compile(buffer);
-  console.log(WebAssembly.Module.imports(module));
   const canvas = document.getElementById("canvas");
-  gl = canvas.getContext("webgl");
+  gl = canvas.getContext("webgl2");
   if (gl === null) {
     console.log('failed to get webgl context :(')
   } else {
     console.log('yay! loaded webgl context :)')
   }// Vertex shader program
-  const vsSource = `
-      attribute vec4 aVertexPosition;
-      void main() {
-        gl_Position = aVertexPosition;
-      }
-    `;
 
-  const fsSource = `
-    void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+
+  const mod = await WebAssembly.instantiateStreaming(fetch("/zig-out/bin/index.wasm"), {
+    env: {
+      compileLinkProgram: compileLinkProgramWasm,
+      bind2DFloat32Data: bind2DFloat32DataWasm,
+      glBindVertexArray: (vao) => gl.bindVertexArray(vaos[vao]),
+      glClearColor: gl.clearColor.bind(gl),
+      glClear: gl.clear.bind(gl),
+      glUseProgram: (program) => {
+        gl.useProgram(programs[program])
+      },
+      glDrawArrays: gl.drawArrays.bind(gl),
     }
-  `;
+  });
+  memory = mod.instance.exports.memory;
+  console.log(mod.instance.exports.add(1, 2));
+  mod.instance.exports.run();
 
-  // Now create an array of positions for the square.
-  const program = compileLinkProgram(gl, vsSource, fsSource);
-  const positions = [0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5];
-  const vao = bind2DFloat32Data(positions, vsSource, fsSource);
-
-
-  gl.clearColor(0.0, 0.0, 1.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  gl.useProgram(program);
-  {
-    const offset = 0;
-    const vertexCount = 4;
-    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-  }
+  // const vsSource = `
+  //     attribute vec4 aVertexPosition;
+  //     void main() {
+  //       gl_Position = aVertexPosition;
+  //     }
+  //   `;
+  //
+  // const fsSource = `
+  //   void main() {
+  //     gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  //   }
+  // `;
+  //
+  // // Now create an array of positions for the square.
+  // const program = compileLinkProgram(vsSource, fsSource);
+  // const positions = new Float32Array([0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5]);
+  // const vao = bind2DFloat32Data(positions, vsSource, fsSource);
+  //
+  //
+  // gl.clearColor(0.0, 0.0, 1.0, 1.0);
+  // gl.clear(gl.COLOR_BUFFER_BIT);
+  //
+  // gl.useProgram(program);
+  // {
+  //   const offset = 0;
+  //   const vertexCount = 4;
+  //   gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+  // }
 };
 
 window.onload = init
--- src/index.zig
+++ src/index.zig
@@ -1,10 +1,10 @@
-// pub extern fn compileLinkProgram(vs: [*]const u8, vs_len: usize, fs: [*]const u8, fs_len: usize) i32;
-// pub extern fn bind2DFloat32Data(data: [*]const f32, data_len: usize) i32;
-// pub extern fn glBindVertexArray(vao: i32) void;
-// pub extern fn glClearColor(r: f32, g: f32, b: f32, a: f32) void;
-// pub extern fn glClear(mask: i32) void;
-// pub extern fn glUseProgram(program: i32) void;
-// pub extern fn glDrawArrays(mode: i32, first: i32, last: i32) void;
+pub extern fn compileLinkProgram(vs: [*]const u8, vs_len: usize, fs: [*]const u8, fs_len: usize) i32;
+pub extern fn bind2DFloat32Data(data: [*]const f32, data_len: usize) i32;
+pub extern fn glBindVertexArray(vao: i32) void;
+pub extern fn glClearColor(r: f32, g: f32, b: f32, a: f32) void;
+pub extern fn glClear(mask: i32) void;
+pub extern fn glUseProgram(program: i32) void;
+pub extern fn glDrawArrays(mode: i32, first: i32, last: i32) void;
 //
 //
 // Taken from web sys rust defintions...
@@ -15,30 +15,30 @@ pub export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
-// pub export fn run() void {
-//     const vs_source =
-//         \\ attribute vec4 aVertexPosition;
-//         \\ void main() {
-//         \\   gl_Position = aVertexPosition;
-//         \\ }
-//     ;
-//
-//     const fs_source =
-//         \\ void main() {
-//         \\   gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-//         \\ }
-//     ;
-//     const program = compileLinkProgram(vs_source, vs_source.len, fs_source, fs_source.len);
-//     const positions: []const f32 = &.{ 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5 };
-//     const vao = bind2DFloat32Data(positions.ptr, positions.len);
-//
-//     glBindVertexArray(vao);
-//     glClearColor(0.0, 0.0, 1.0, 1.0);
-//     glClear(COLOR_BUFFER_BIT);
-//     glUseProgram(program);
-//     {
-//         const offset = 0;
-//         const vertexCount = 4;
-//         glDrawArrays(TRIANGLE_STRIP, offset, vertexCount);
-//     }
-// }
+pub export fn run() void {
+    const vs_source =
+        \\ attribute vec4 aVertexPosition;
+        \\ void main() {
+        \\   gl_Position = aVertexPosition;
+        \\ }
+    ;
+
+    const fs_source =
+        \\ void main() {
+        \\   gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+        \\ }
+    ;
+    const program = compileLinkProgram(vs_source, vs_source.len, fs_source, fs_source.len);
+    const positions: []const f32 = &.{ 0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5 };
+    const vao = bind2DFloat32Data(positions.ptr, positions.len);
+
+    glBindVertexArray(vao);
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(COLOR_BUFFER_BIT);
+    glUseProgram(program);
+    {
+        const offset = 0;
+        const vertexCount = 4;
+        glDrawArrays(TRIANGLE_STRIP, offset, vertexCount);
+    }
+}
